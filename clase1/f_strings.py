c2c624b5c8ada434e25a84e7ee53875f8a3f5865
Alison Herrera <yaliherrera02@gmail.com>
2024-08-23 11:15:07 -0300

    segui trabajando ej1.parte6

--- clase1/f_strings.py
+++ clase1/f_strings.py
@@ -11,3 +11,9 @@ print(f'esta es mi lista random {["Python",True,5,("a","b")]}')
 nombre="Julia"
 edad=35
 print(f'Hola mi nombre es {nombre} y tengo {edad} años')
+
+precio = 49.9999
+print(f'El precio con dos decimales es {precio:.2f}.')
+
+numero =3542.3924239423
+print (f'el numero redondeado es: {numero:.1f}')
